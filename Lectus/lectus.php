58f83518a44322e084813b98d3a57c84ca113a16
mpachetta <mpachetta@hotmail.com>
2021-02-02 00:17:11 -0300

    funciona session y login

--- Lectus/lectus.php
+++ Lectus/lectus.php
@@ -102,6 +102,7 @@
 
             </div>
             <button id="jugar" disabled=true>JUGAR</button>
+            <button class="b_usuname" id="salir"><a href="../comprueba_login\cerrar_sesion.php">Salir del juego</a></button>
         </div>
 
 
--- comprueba_login/cerrar_sesion.php
+++ comprueba_login/cerrar_sesion.php
@@ -2,7 +2,7 @@
 session_start();
 session_destroy();
 
-echo "La sesión ha finalizado";
-
+echo "La sesión ha finalizado.<br><br>";
+echo "<a href='../Lectus\index.php'>Continuar</a>"
 
 ?>
